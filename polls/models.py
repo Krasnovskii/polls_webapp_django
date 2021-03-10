413a972a3b1808cae9a6e1f551a4e2d524bb2411
Krasnovskii <krasnovskiisv@gmail.com>
2021-03-10 07:58:42 +0000

    webapp v5

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,15 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    def __str__(self):
+           return self.user.username
+
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=250)
@@ -27,3 +36,10 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
+class Answer(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, default=None)
+    question = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
+    choice = models.ForeignKey(Choice, on_delete=models.DO_NOTHING)
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.question.name
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, request
 from django.urls import reverse
-from .models import Question, Choice
+from .models import Question, Choice, UserProfile, User, Answer
 from django.views import generic
 from django.utils import timezone
 
@@ -15,9 +15,26 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+    # Пытаюсь вставить отбор по дате и по юзеру, но получаю огромное количество ошибок....
+    # def latest_question_list():
+    #
+    #     context = {}
+    #     # Get user profile
+    #     get_user = get_object_or_404(UserProfile, user=request.user)
+    #     # get all answers of user
+    #     user_answers = Answer.objects.all().filter(user=get_user)
+    #     # filter questions of pub date
+    #     questions = Question.objects.filter(pub_date__lte=timezone.now())
+    #     latest_question_list = []
+    #     for user in user_answers:
+    #         for question in questions:
+    #             if not user.id in questions:
+    #                 latest_question_list.append(question)
+    #     context['index'] = latest_question_list
+    #
+    #     return render(request, 'index.html', context)
 
 
 class DetailView(generic.DetailView):
